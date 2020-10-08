e8f9d47028a74655d4baf11947d9ee9082f8184b
chrisokbimpong <chris.bimpong@amalitech.org>
2020-10-08 12:12:23 +0000

    Third commit (leveraging open trivia API to populate quiz application with questions)

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -7,32 +10,32 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-{
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1
-},
-{
-    question: "What is the correct syntax for referring to an extenal script called 'xxx.js'?",
-    choice1: "<script href'xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3
-},
-{
-    question: "How do you write 'Hello world' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
-}
-];
+let questions = [];
+fetch("https://opentdb.com/api.php?amount=20&category=31&difficulty=easy&type=multiple")
+.then(res => {
+    return res.json();
+}).then(loadedQuestions => {
+    console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map(loadedQuestion => {
+        const formattedQuestion = {
+            question: loadedQuestion.question
+        };
+
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+        answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
+
+        answerChoices.forEach((choice, index) => {
+            formattedQuestion["choice" + (index+1)] = choice;
+        })
+        return formattedQuestion;
+    });
+    // questions = loadedQuestions;
+    startGame();
+})
+.catch(err => {
+    console.error(err);
+});
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -48,10 +51,16 @@ startGame = () => {
 getNewQuestion = () => {
 
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
         // go to the end page
         return window.location.assign("/end.html");
     }
     questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+    //Progress bar update
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -75,6 +84,9 @@ choices.forEach(choice => {
         const selectedAnswer = selectedChoice.dataset["number"];
 
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        if(classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
         
         selectedChoice.parentElement.classList.add(classToApply);
         setTimeout( () => {
@@ -84,4 +96,8 @@ choices.forEach(choice => {
     });
 });
 
-startGame();
+incrementScore = num =>{
+    score += num;
+    scoreText.innerText = score;
+}
+
